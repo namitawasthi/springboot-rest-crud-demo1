05703227d3cdb40fd66cbacd50660bd8c787e7b4
Namit <namitawasthi511@gmail.com>
2020-01-15 22:39:16 +0530

    first commit

---
+++ src/main/java/com/namit/springboot/springbootreactcrud/SpringbootReactCrudApplication.java
@@ -0,0 +1,13 @@
+package com.namit.springboot.springbootreactcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/namit/springboot/springbootreactcrud/SpringbootReactCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namit.springboot.springbootreactcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
